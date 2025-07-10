b2128a0108aec4c6d170415879ba782b990ed0a4
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-07-10 14:17:59 +0530

    Create a class to manage Icons

---
+++ SearchEnginesExtension/Library/Icons.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CommandPalette.Extensions.Toolkit;
+
+namespace SearchEnginesExtension
+{
+    internal static class Icons
+    {
+        public static IconInfo WebSearch { get; } = new IconInfo("\uF6FA");
+    }
+}
