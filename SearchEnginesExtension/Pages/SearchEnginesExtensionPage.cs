25e4f34e63a2e60259514594d8214b3da5b4ec64
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-07-10 13:43:00 +0530

    Hardcode a list of Search Engines and create `ListItems`

--- SearchEnginesExtension/Pages/SearchEnginesExtensionPage.cs
+++ SearchEnginesExtension/Pages/SearchEnginesExtensionPage.cs
@@ -2,6 +2,7 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
 using Microsoft.CommandPalette.Extensions;
 using Microsoft.CommandPalette.Extensions.Toolkit;
 
@@ -9,6 +10,13 @@ namespace SearchEnginesExtension;
 
 internal sealed partial class SearchEnginesExtensionPage : ListPage
 {
+    private List<SearchEngine> searchEngines = new()
+    {
+        new SearchEngine { Name = "Google", Url = "https://www.google.com/search?q=", Shortcut = "google" },
+        new SearchEngine { Name = "Bing", Url = "https://www.bing.com/search?q=", Shortcut = "bing" },
+        new SearchEngine { Name = "DuckDuckGo", Url = "https://duckduckgo.com/?q=", Shortcut = "duckduckgo" },
+    };
+
     public SearchEnginesExtensionPage()
     {
         Icon = IconHelpers.FromRelativePath("Assets\\StoreLogo.png");
@@ -18,8 +26,11 @@ internal sealed partial class SearchEnginesExtensionPage : ListPage
 
     public override IListItem[] GetItems()
     {
-        return [
-            new ListItem(new NoOpCommand()) { Title = "TODO: Implement your extension here" }
-        ];
+        return searchEngines.ConvertAll(engine => new ListItem(new OpenUrlCommand($"{engine.Url}"))
+        {
+            Title = engine.Name,
+            Subtitle = $"Search using {engine.Name}",
+            Icon = IconHelpers.FromRelativePath("Assets\\SearchIcon.png"),
+        }).ToArray();
     }
 }
