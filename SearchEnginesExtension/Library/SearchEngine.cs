d02253c27e485a8922ce07f33178e861bb92fe5d
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-07-19 14:08:37 +0530

    Create utility method to generate Search URLs

--- SearchEnginesExtension/Library/SearchEngine.cs
+++ SearchEnginesExtension/Library/SearchEngine.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using System.Text.Json.Serialization;
+using System.Net;
 
 namespace SearchEnginesExtension
 {
@@ -15,6 +15,18 @@ namespace SearchEnginesExtension
         public required string Url { get; set; }
 
         public required string Shortcut { get; set; }
+
+        /// <summary>
+        /// Generates the search url for the given query for this search engine
+        /// </summary>
+        /// <param name="query">The thing to search for</param>
+        /// <returns>A URL formatted for the search engine</returns>
+        public string Search(string query)
+        {
+            var encodedQuery = WebUtility.UrlEncode(query);
+            var searchUrl = Url.Replace("%s", encodedQuery);
+            return searchUrl;
+        }
     }
 
     [JsonSourceGenerationOptions(
