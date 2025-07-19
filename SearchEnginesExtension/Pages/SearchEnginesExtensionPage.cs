920214d01041e324a4eda9bece24f7e364b0fd1e
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-07-19 14:17:42 +0530

    Make the main `SearchEnginesExtensionPage` dynamic

--- SearchEnginesExtension/Pages/SearchEnginesExtensionPage.cs
+++ SearchEnginesExtension/Pages/SearchEnginesExtensionPage.cs
@@ -3,13 +3,26 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using Microsoft.CommandPalette.Extensions;
 using Microsoft.CommandPalette.Extensions.Toolkit;
 
-namespace SearchEnginesExtension;
+﻿namespace SearchEnginesExtension;
 
-internal sealed partial class SearchEnginesExtensionPage : ListPage
+/// <summary>
+/// The page that displays the search engines
+/// </summary>
+internal sealed partial class SearchEnginesExtensionPage : DynamicListPage
 {
+    /// <summary>
+    /// The list of items to display
+    /// </summary>
+    List<ListItem> items = [];
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SearchEnginesExtensionPage"/> class
+    /// </summary>
     public SearchEnginesExtensionPage()
     {
         Icon = Icons.WebSearch;
@@ -18,13 +31,91 @@ internal sealed partial class SearchEnginesExtensionPage : ListPage
         Configuration.Load();
     }
 
+    /// <summary>
+    /// Tells CommandPalette which items to display in the list
+    /// </summary>
+    /// <returns>The search engines items to display</returns>
     public override IListItem[] GetItems()
     {
-        return Configuration.SearchEngines.ConvertAll(engine => new ListItem(new OpenUrlCommand($"{engine.Url}"))
+        // If the items list is not empty (dynamically generated), then display them
+        if (items.Count > 0)
         {
-            Title = engine.Name,
-            Subtitle = $"Search using {engine.Name}",
-            Icon = Icons.WebSearch,
-        }).ToArray();
+            return items.ToArray();
+        }
+        // Otherwise, fallback to the list of all search engines
+        else
+        {
+            return Configuration.SearchEngines.ConvertAll(engine => {
+                string homeUrl = engine.Url.Replace("%s", "");
+                return new ListItem(new OpenUrlCommand(homeUrl))
+                {
+                    Title = engine.Name,
+                    Subtitle = $"Go to {engine.Name}",
+                    Icon = Icons.WebSearch,
+                };
+            }).ToArray();
+        }
+    }
+    
+    /// <summary>
+    /// Fires off everytime the CommandPalette search text is updated
+    /// </summary>
+    /// <param name="oldSearch">The previous search text</param>
+    /// <param name="newSearch">The current search text</param>
+    public override void UpdateSearchText(string oldSearch, string newSearch)
+    {
+        // If the search text is the same, do nothing.
+        if (oldSearch == null || newSearch == null) return;
+        if (oldSearch ==  newSearch) return;
+
+        // If the search text is empty, clear the items. (To show all list items)
+        if (string.IsNullOrEmpty(newSearch))
+        {
+            items.Clear();
+        }
+        // If the search text contains a shortcut, filter by the shortcut
+        else if (newSearch.Contains('!'))
+        {
+            // Split the search text into the shortcut and the query
+            var queryParts = newSearch.Split(' ', 2);
+            var shortcut = queryParts[0][1..];
+
+            // Fuzzy search for the shortcut and order by the score
+            items = Configuration.SearchEngines
+                .Select(engine => (engine, score: StringMatcher.FuzzySearch(shortcut, engine.Shortcut).Score))
+                .Where(result => result.score > 0)
+                .OrderByDescending(result => result.score)
+                .Select(result =>
+                {
+                    // Create a new list item for the search engine
+                    var engine = result.engine;
+                    var query = queryParts.Length > 1 ? queryParts[1] : string.Empty;
+                    var searchUrl = engine.Search(query);
+                    return new ListItem(new OpenUrlCommand(searchUrl))
+                    {
+                        Title = engine.Name,
+                        Subtitle = searchUrl,
+                        Icon = Icons.WebSearch,
+                    };
+                }).ToList();
+        }
+        // Otherwise, search all search engines
+        else
+        {
+            items = Configuration.SearchEngines.Select(engine =>
+            {
+                // Create a new list item for the search engine
+                var searchUrl = engine.Search(newSearch);
+                return new ListItem(new OpenUrlCommand(searchUrl))
+                {
+                    Title = engine.Name,
+                    Subtitle = searchUrl,
+                    Icon = Icons.WebSearch,
+                };
+            }).ToList();
+        }
+
+        // Raise the items changed event (This notifies CmdPal that the DynamicList has changed)
+        RaiseItemsChanged(items.Count);
     }
 }
